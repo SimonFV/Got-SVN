751c57dfbd0ee5039a9ddd9b13a3dd7dbb479101
SimonFV <sfv0215@gmail.com>
2020-07-29 19:16:35 -0600

    GS-2 Añadida la conexión entre el cliente y el API.

---
+++ client/include/client.hpp
@@ -0,0 +1,9 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <iostream>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include <cpr/cpr.h>
+
+#endif
---
+++ client/src/main.cpp
@@ -0,0 +1,53 @@
+
+#include <client.hpp>
+
+int main()
+{
+    /*
+    //POST
+    {
+        std::cout << "Action: Create Product with Id = 1" << std::endl;
+        auto r = cpr::Post(cpr::Url{"http://localhost:51654/api/products/create"},
+                           cpr::Body{R"({"Id":1, 
+            "Name":"ElectricFan","Qty":14,"Price":20.90})"},
+                           cpr::Header{{"Content-Type", "application/json"}});
+        std::cout << "Returned Status:" << r.status_code << std::endl;
+    }
+    */
+
+    //GET
+    {
+        spdlog::info("GET REQUEST");
+
+        auto r = cpr::Get(cpr::Url{"http://localhost:3000"});
+
+        spdlog::info("Returned Text: {}", r.text);
+    }
+
+    /*
+    {
+        std::cout << "Action: Update Product with Id = 1" << std::endl;
+        auto r = cpr::Post(cpr::Url{"http://localhost:51654/api/products/1"},
+                           cpr::Body{R"({"Id":1, 
+            "Name":"ElectricFan","Qty":15,"Price":29.80})"},
+                           cpr::Header{{"Content-Type", "application/json"}});
+        std::cout << "Returned Status:" << r.status_code << std::endl;
+    }
+    {
+        std::cout << "Action: Retrieve all products" << std::endl;
+        auto r = cpr::Get(cpr::Url{"http://localhost:51654/api/products"});
+        std::cout << "Returned Text:" << r.text << std::endl;
+    }
+    {
+        std::cout << "Action: Delete the product with id = 1" << std::endl;
+        auto r = cpr::Delete(cpr::Url{"http://localhost:51654/api/products/1"});
+        std::cout << "Returned Status:" << r.status_code << std::endl;
+    }
+    {
+        std::cout << "Action: Retrieve all products" << std::endl;
+        auto r = cpr::Get(cpr::Url{"http://localhost:51654/api/products"});
+        std::cout << "Returned Text:" << r.text << std::endl;
+    }
+    */
+    return 0;
+}
