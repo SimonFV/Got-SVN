77031b18224ae819e00b21a2e91d3a9e3cb62217
davidaqc <davidqc05@gmail.com>
2020-08-09 23:58:58 -0600

    Optimizando sistema control

--- client/src/commands.cpp
+++ client/src/commands.cpp
@@ -79,17 +79,14 @@ void Command::init(string repoName)
     Json::Value root;
     root["commit"] = 0;
     root["hash_commit"] = "";
-    root["archivos_agregados"];
-    root["archivos_del_server"];
-    root["archivos_posibles_modificados"];
-    root["archivos_del_cliente"];
+    root["archivos"];
     control.escribir_json(".got/control_cliente.json", root);
 
     // Agregar directorios al json
     control.list_dir(thisPath);
 }
 
-void Command::add(string archivos)
+void Command::add(string solicitud_archivos)
 {
     // Antes de agregar agregar los archivos, actualizar directorio
     control.list_dir(Command::thisPath);
@@ -99,20 +96,57 @@ void Command::add(string archivos)
     Json::Reader reader;
     Json::Value root;
     reader.parse(ifs, root);
+    bool archivo_existente = true;
 
-    if (archivos == "-A")
+    if (solicitud_archivos == "-A")
     {
-        // Agregar todos
-        string aux = "archivos_del_cliente";
+        string aux = "archivos";
         for (Json::Value::const_iterator it = root[aux].begin(); it != root[aux].end(); ++it)
         {
-            control.control_local_server(it.key().asString(), "");
+
+            if (root["archivos"][it.key().asString()] == "no_controlado")
+            {
+                root["archivos"][it.key().asString()] = "agregado";
+                spdlog::info("Archivo agregado!");
+            }
+            else if (root["archivos"][it.key().asString()] == "server")
+            {
+                root["archivos"][it.key().asString()] = "modificado";
+                spdlog::info("El archivo ha sido modificado!");
+            }
+            else if (root["archivos"][it.key().asString()] == "agregado")
+            {
+                spdlog::info("El archivo ya ha sido agregado!");
+            }
         }
     }
     else
     {
         //Agregar uno
-        control.control_local_server(archivos, "");
+        if (root["archivos"][solicitud_archivos] == "no_controlado")
+        {
+            root["archivos"][solicitud_archivos] = "agregado";
+            spdlog::info("Archivo agregado!");
+        }
+        else if (root["archivos"][solicitud_archivos] == "server")
+        {
+            root["archivos"][solicitud_archivos] = "modificado";
+            spdlog::info("El archivo ha sido modificado!");
+        }
+        else if (root["archivos"][solicitud_archivos] == "agregado")
+        {
+            spdlog::info("El archivo ya ha sido agregado!");
+        }
+        else
+        {
+            archivo_existente = false;
+            spdlog::info("El archivo no existe!");
+        }
+    }
+
+    if (archivo_existente == true)
+    {
+        control.escribir_json(".got/control_cliente.json", root);
     }
 }
 
@@ -131,6 +165,7 @@ void Command::commit(string comentario)
     root["comentario"] = comentario;
     control.escribir_json(".got/enviado.json", root);
     Client::getI()->POST("commit", thisPath + ".got/enviado.json");
+    spdlog::info("Commit agregado en el server!");
     spdlog::info(Client::getI()->getStatus());
 
     // Guardar HASH generado
@@ -145,124 +180,128 @@ void Command::commit(string comentario)
     root1["commit"] = relacion_actual_commit;
     control.escribir_json(".got/control_cliente.json", root1);
 
-    // Enviar archivos agregados
-    string aux = "archivos_agregados";
-    for (Json::Value::const_iterator it = root1[aux].begin(); it != root1[aux].end(); ++it)
-    {
-        spdlog::info("Agregando archivos al repositorio!");
-        // Comprimir datos
-        string texto_final = generar_string_de_archivo(Command::thisPath + it.key().asString());
-        buildHuffmanTree(texto_final);
-        string cod_binario = pedir_codigoBinario();
-        string sim_cod = pedir_simboloCodigo();
-
-        // Escribe la estructura  archivo y envia datos
-        Json::Value root2;
-        root2["nombre_archivo"] = it.key().asString();
-        root2["codigo_huffman"] = cod_binario;
-        root2["simbolo_codigo"] = sim_cod;
-        root2["relacion_commit"] = relacion_actual_commit;
-        control.escribir_json(".got/enviado.json", root2);
-
-        Client::getI()->POST("archivo", thisPath + ".got/enviado.json");
-        spdlog::info(Client::getI()->getStatus());
-
-        // Agregar archivos de "archivos_agregados" a "archivos_del_server"
-        root1["archivos_del_server"][it.key().asString()];
-        control.escribir_json(".got/control_cliente.json", root1);
-    }
-
-    // Enviar archivos modificados
-    aux = "archivos_posibles_modificados";
+    // Enviar archivos
+    string aux = "archivos";
+    string texto_final;
+    string cod_binario;
+    string sim_cod;
     for (Json::Value::const_iterator it = root1[aux].begin(); it != root1[aux].end(); ++it)
     {
 
-        string comparacion_diff;
-        if (relacion_actual_commit == 2)
+        if (root1["archivos"][it.key().asString()] == "agregado")
         {
-            spdlog::info("Agregando diff's del commit 2!");
-            // Pedir Huffman del commit 1
-            Json::Value root3;
-            root3["id_commit"] = 1; // Directamente al commit 1
-            root3["nombre_archivo"] = it.key().asString();
-            control.escribir_json(".got/enviado.json", root3);
-            Client::getI()->GET("codigo_huffman", thisPath + ".got/enviado.json");
+            // Comprimir datos
+            texto_final = generar_string_de_archivo(Command::thisPath + it.key().asString());
+            buildHuffmanTree(texto_final);
+            cod_binario = pedir_codigoBinario();
+            sim_cod = pedir_simboloCodigo();
+
+            // Escribe la estructura  archivo y envia datos
+            Json::Value root2;
+            root2["nombre_archivo"] = it.key().asString();
+            root2["codigo_huffman"] = cod_binario;
+            root2["simbolo_codigo"] = sim_cod;
+            root2["relacion_commit"] = relacion_actual_commit;
+            control.escribir_json(".got/enviado.json", root2);
+
+            Client::getI()->POST("archivo", thisPath + ".got/enviado.json");
+            spdlog::info("Archivo agregado en el server!");
             spdlog::info(Client::getI()->getStatus());
-            // Guardar hufmman
-            dato_retornado = control.leer_json2(".got/recibido.json", "CONVERT(archivo.codigo_huffman USING utf8)");
-            dato_retornado2 = control.leer_json2(".got/recibido.json", "simbolo_codigo");
-            // Crear archivo con huffman
-            ofstream fs(Command::thisPath + "../repo/" + it.key().asString());
-            fs << descomprimir_data(dato_retornado, dato_retornado2);
-            fs.close();
 
-            // Saca diferencias del Antes con el Despues. Poner RUTA DESPUES, LUEGO RUTA ANTES
-            Command::diff(Command::thisPath + it.key().asString(),
-                          Command::thisPath + "../repo/" + it.key().asString(),
-                          Command::thisPath + "../repo/test.patch");
-
-            comparacion_diff = Command::diff_a_string();
+            // Cambiar estado agregado -> server
+            root1["archivos"][it.key().asString()] = "server";
+            control.escribir_json(".got/control_cliente.json", root1);
         }
-        else
-        {   
-            spdlog::info("Agregando diff's del commit 2!");
-            // Pedir diferencias (diff anterior)
-            // Escribe la estructura diff_anterior y envia datos
-            Json::Value root4;
-            root4["id_commit"] = relacion_actual_commit - 1; // Se resta 1 para obtener diff_anterior
-            root4["nombre_archivo"] = it.key().asString();
-            control.escribir_json(".got/enviado.json", root4);
-            Client::getI()->GET("commit_anterior", thisPath + ".got/enviado.json");
-            spdlog::info(Client::getI()->getStatus());
-            // Obtener codigo_diff_anterior
-            dato_retornado = control.leer_json2(".got/recibido.json", "codigo_diff_anterior");
-
-            // Para revisar si hay cambios
-            // Copiar codigo_diff_anterio en test.patch
-            ofstream fs(Command::thisPath + "../repo/test.patch");
-            fs << dato_retornado;
-            fs.close();
-
-            // Regresar el archivo al commmit pasado
-            Command::applyChanges(Command::thisPath + "../repo/" + it.key().asString(),
-                                  Command::thisPath + "../repo/test.patch");
-
-            // Compararlo con el actual
-            Command::diff(Command::thisPath + it.key().asString(),
-                          Command::thisPath + "../repo/" + it.key().asString(),
-                          Command::thisPath + "../repo/test.patch");
-
-            comparacion_diff = Command::diff_a_string();
-        }
-
-        // Si el archivo .patch esta en blanco, no hay diferencia
-        if (comparacion_diff == "")
+        else if (root1["archivos"][it.key().asString()] == "modificado")
         {
-            spdlog::info("No hay cambios en el archivo!");
-        }
-        else
-        {
-            // Sacar MD5
-            string codigo_checksum = md5(to_string(relacion_actual_commit));
+            string comparacion_diff;
+            if (relacion_actual_commit == 2)
+            {
+                spdlog::info("Agregando diff's del commit 2!");
+                // Pedir Huffman del commit 1
+                Json::Value root3;
+                root3["id_commit"] = 1; // Directamente al commit 1
+                root3["nombre_archivo"] = it.key().asString();
+                control.escribir_json(".got/enviado.json", root3);
+                Client::getI()->GET("codigo_huffman", thisPath + ".got/enviado.json");
+                spdlog::info("Obteniendo codigo huffman!");
+                spdlog::info(Client::getI()->getStatus());
+                // Guardar hufmman
+                dato_retornado = control.leer_json2(".got/recibido.json", "CONVERT(archivo.codigo_huffman USING utf8)");
+                dato_retornado2 = control.leer_json2(".got/recibido.json", "simbolo_codigo");
+                // Crear archivo con huffman
+                ofstream fs(Command::thisPath + "../repo/" + it.key().asString());
+                fs << descomprimir_data(dato_retornado, dato_retornado2);
+                fs.close();
 
-            // Escribe la estructura diff y envia datos
-            Json::Value root5;
-            root5["nombre_archivo"] = it.key().asString();
-            root5["codigo_diff_anterior"] = comparacion_diff;
-            root5["codigo_diff_posterior"] = ""; // Va vacio, el proximo commit lo actualiza
-            root5["checksum"] = codigo_checksum;
-            root5["relacion_commit"] = relacion_actual_commit;
-            control.escribir_json(".got/enviado.json", root5);
+                // Saca diferencias del Antes con el Despues. Poner RUTA DESPUES, LUEGO RUTA ANTES
+                Command::diff(Command::thisPath + it.key().asString(),
+                              Command::thisPath + "../repo/" + it.key().asString(),
+                              Command::thisPath + "../repo/test.patch");
 
-            Client::getI()->POST("diff", thisPath + ".got/enviado.json");
-            spdlog::info(Client::getI()->getStatus());
+                comparacion_diff = Command::diff_a_string();
+            }
+            else
+            {
+                spdlog::info("Agregando diff's del commit 2!");
+                // Pedir diferencias (diff anterior)
+                // Escribe la estructura diff_anterior y envia datos
+                Json::Value root4;
+                root4["id_commit"] = relacion_actual_commit - 1; // Se resta 1 para obtener diff_anterior
+                root4["nombre_archivo"] = it.key().asString();
+                control.escribir_json(".got/enviado.json", root4);
+                Client::getI()->GET("commit_anterior", thisPath + ".got/enviado.json");
+                spdlog::info("Obteniendo codigo diff!");
+                spdlog::info(Client::getI()->getStatus());
+                // Obtener codigo_diff_anterior
+                dato_retornado = control.leer_json2(".got/recibido.json", "codigo_diff_anterior");
+
+                // Para revisar si hay cambios
+                // Copiar codigo_diff_anterio en test.patch
+                ofstream fs(Command::thisPath + "../repo/test.patch");
+                fs << dato_retornado;
+                fs.close();
+
+                // Regresar el archivo al commmit pasado
+                Command::applyChanges(Command::thisPath + "../repo/" + it.key().asString(),
+                                      Command::thisPath + "../repo/test.patch");
+
+                // Compararlo con el actual
+                Command::diff(Command::thisPath + it.key().asString(),
+                              Command::thisPath + "../repo/" + it.key().asString(),
+                              Command::thisPath + "../repo/test.patch");
+
+                comparacion_diff = Command::diff_a_string();
+            }
+
+            // Si el archivo .patch esta en blanco, no hay diferencia
+            if (comparacion_diff == "")
+            {
+                spdlog::info("No hay cambios en el archivo!");
+            }
+            else
+            {
+                // Sacar MD5
+                string codigo_checksum = md5(to_string(relacion_actual_commit));
+
+                // Escribe la estructura diff y envia datos
+                Json::Value root5;
+                root5["nombre_archivo"] = it.key().asString();
+                root5["codigo_diff_anterior"] = comparacion_diff;
+                root5["codigo_diff_posterior"] = ""; // Va vacio, el proximo commit lo actualiza
+                root5["checksum"] = codigo_checksum;
+                root5["relacion_commit"] = relacion_actual_commit;
+                control.escribir_json(".got/enviado.json", root5);
+
+                Client::getI()->POST("diff", thisPath + ".got/enviado.json");
+                spdlog::info("Diff agregado en el server!");
+                spdlog::info(Client::getI()->getStatus());
+            }
+            
+            // Cambiar estado modificado -> server
+            root1["archivos"][it.key().asString()] = "server";
+            control.escribir_json(".got/control_cliente.json", root1);
         }
     }
 
-    // Quitar archivos de "archivos_agregados" y "archivos_posibles_modificados"
-    root1.removeMember("archivos_agregados");
-    root1.removeMember("archivos_posibles_modificados");
-    root1["archivos_agregados"];
-    root1["archivos_posibles_modificados"];
-    control.escribir_json(".got/control_cliente.json", root1);
 }
